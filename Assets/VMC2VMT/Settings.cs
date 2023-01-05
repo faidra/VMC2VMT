9a03f2b1735728dc8da704f14a7071d9fd1be210
faidra <faidra.windmark@gmail.com>
2023-01-06 03:46:16 +0900

    SettingsにLogger追加

--- Assets/VMC2VMT/Settings.cs
+++ Assets/VMC2VMT/Settings.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using UnityEngine;
 using Newtonsoft.Json;
@@ -9,14 +10,25 @@ namespace VMC2VMT
     public sealed class Settings : MonoBehaviour
     {
         [SerializeField] string fileName;
+
+        [SerializeField] Logger logger;
+
         SettingsType settings;
 
         public void ReadSettings(bool force = false)
         {
             if (!force && settings != null) return;
-            using var file = File.OpenText(Path.Combine(Application.streamingAssetsPath, fileName));
-            using var reader = new JsonTextReader(file);
-            settings = JsonSerializer.CreateDefault().Deserialize<SettingsType>(reader);
+            try
+            {
+                using var file = File.OpenText(Path.Combine(Application.streamingAssetsPath, fileName));
+                using var reader = new JsonTextReader(file);
+                settings = JsonSerializer.CreateDefault().Deserialize<SettingsType>(reader);
+            }
+            catch (Exception e)
+            {
+                logger.AddLogError(e.Message);
+                throw;
+            }
         }
 
         public bool TryGet(string name, out int value)
